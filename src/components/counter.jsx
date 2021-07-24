77989b85e1be775a6f46b3f78e9fe8c09f5f5f51
Asad Ali <emailisasad@gmail.com>
2021-07-24 10:53:50 +0500

    initial commit.

---
+++ src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    counter: 0,
+  };
+
+  incrementClicked() {
+    console.log("Increment button clicked!");
+  }
+
+  render() {
+    return (
+      <div>
+        <span
+          style={{ backgroundColor: "blue" }}
+          className="badge badge-primary m-3"
+        >
+          {this.incerementCounter()}
+        </span>
+        <button
+          onClick={this.incrementClicked}
+          className="btn btn-secondary btn-sm m-3"
+        >
+          Increment
+        </button>
+      </div>
+    );
+  }
+
+  incerementCounter() {
+    const { counter } = this.state;
+    return counter === 0 ? "Zero" : counter;
+  }
+}
+
+export default Counter;
